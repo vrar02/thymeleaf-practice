1156179c2c4a874a65a1b216c9eb9ebf4c10b26e
Vivek Reddy <vivekanthireddy02@gmail.com>
2023-03-09 23:34:32 -0600

    basic iteration on list, using standard expression language with the help of thymeleaf xmlns
    th:text
    th:with for declaring the variables
    th:each for iterating in a loop
    
    about page handler

--- src/main/java/com/vivek/thymeleafpractice/controllers/MyFirstController.java
+++ src/main/java/com/vivek/thymeleafpractice/controllers/MyFirstController.java
@@ -1,18 +1,28 @@
 package com.vivek.thymeleafpractice.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 
 @Controller
 public class MyFirstController {
 
     @RequestMapping(value = "/about", method = RequestMethod.GET)
-    public String aboutPage(){
+    public String aboutPage(Model model){
+        model.addAttribute("msg","Hi..Welcome to Thymleaf ");
+        model.addAttribute("currentdate", new Date().toString());
+        List<String> names=new ArrayList<>(List.of("vivek","ravi","pruthvi"));
         System.out.println("in about handler");
+        model.addAttribute("names",names);
         return "about";
     }
+    
 
 }
